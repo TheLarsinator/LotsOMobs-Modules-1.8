7c79473d73860b3f8c6d309e44b1f5c1b80e4820
TheLarsinator <TheLarsinatormodding@hotmail.com>
2015-12-20 10:20:14 +0100

    Christmas, lets work at home

--- LotsOMobs Core/src/main/java/thelarsinator/lomjungleandswamps/init/LotsOMobsJungleAndSwampsMobs.java
+++ LotsOMobs Core/src/main/java/thelarsinator/lomjungleandswamps/init/LotsOMobsJungleAndSwampsMobs.java
@@ -31,13 +31,13 @@ public class LotsOMobsJungleAndSwampsMobs {
 		}
 		if(ConfigHandler.FrogOn == true)
 		{
-	    	EntityRegistry.registerGlobalEntityID(EntityFrog.class, mobid + "Frog", EntityRegistry.findGlobalUniqueEntityId(), 0xDEC2A2, 0x506E21);
+	    	EntityRegistry.registerGlobalEntityID(EntityFrog.class, mobid + "Frog", EntityRegistry.findGlobalUniqueEntityId(), 0x1A8C0F, 0x75A807);
 	    	EntityRegistry.addSpawn(EntityFrog.class,  (int)(ConfigHandler.FrogRate *10),  4,  5,  EnumCreatureType.CREATURE, new BiomeGenBase[] {BiomeGenBase.swampland, BiomeGenBase.jungle});
         	EntityRegistry.addSpawn(EntityFrog.class, (int)(ConfigHandler.FrogRate *10), 1, 4, EnumCreatureType.CREATURE, BiomeDictionary.getBiomesForType(Type.SWAMP));
 		}
 		if(ConfigHandler.FlyOn == true)
 		{
-	    	EntityRegistry.registerGlobalEntityID(EntityFly.class, mobid + "Fly", EntityRegistry.findGlobalUniqueEntityId(), 0xDEC2A2, 0x506E21);
+	    	EntityRegistry.registerGlobalEntityID(EntityFly.class, mobid + "Fly", EntityRegistry.findGlobalUniqueEntityId(), 0x000000, 0x5ED0F2);
 	    	EntityRegistry.addSpawn(EntityFly.class,  (int)(ConfigHandler.FlyRate *10),  4,  5,  EnumCreatureType.CREATURE, new BiomeGenBase[] {BiomeGenBase.swampland, BiomeGenBase.jungle});
         	EntityRegistry.addSpawn(EntityFly.class, (int)(ConfigHandler.FlyRate *10), 1, 4, EnumCreatureType.CREATURE, BiomeDictionary.getBiomesForType(Type.SWAMP));
 		}
